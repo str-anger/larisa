929004543b98564f2ed9bd05952fc6f3fad0bb26
Nils Wilhelm Becker <nilbecke@mail.uni-mannheim.de>
2021-10-05 11:59:29 +0200

    #9 Refractoring code to integrate in request loop

--- larisa.py
+++ larisa.py
@@ -5,95 +5,6 @@ import speech_recognition as sr
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator as IAMA
 import sounddevice
-import requests
-from geopy import Nominatim
-
-
-def temperature_conversion(temp):
-    '''
-    #Author: Nils Becker
-    Converts a given temperature in °K into a °C String representation
-    :param temp: Float or int Value of temperature in °K
-    :returns: The temperature converted to °C as a String
-    '''
-    try:
-        return f"{int(temp)-273}°C"
-    except ValueError:
-        logging.error(f"{temp} must be an int or float")
-        pass
-
-def weather_forecast(cfg, city):
-    '''
-    #Author: Nils Becker
-    Gives a weather forecast for a specified city
-    :param cfg: Currently used configuration
-    :param city: String representation of city
-    :returns: weather forecast for now/ next 12h/ next 7 days in a List
-    '''
-    locator = Nominatim(user_agent="Larissa")
-    location = locator.geocode(city)
-    #get key from https://openweathermap.org/
-    api_key = cfg['weather']['apikey']
-    try:
-        #create API request for given city
-        url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&appid={}'.format(location.latitude, location.longitude, "minutely,alerts", api_key)
-        response = requests.get(url).json()
-    except AttributeError:
-        logging.error(f"City {city} not found!")
-        return []
-    result = []
-    try:
-        #get data
-        current = response['curent']
-        currentList = []
-        temperature = temperature_conversion(current['temp'])
-        weather = current['weather']
-        description = weather[0]['description']
-        clouds = f"{current['clouds']}% clouds"
-        currentList.append(temperature)
-        currentList.append(description)
-        currentList.append(clouds)
-        result.append([currentList])
-        for option in ["hourly", "daily"]:
-            #get hourly, then daily data
-
-            currentList = []
-            current = response[option]
-            #if option == hourly, the next 12 hours are relevant -> length = 13
-            #if option == daily, the next 7 days are relevant -> length 8
-            length = 13 if option == "hourly" else 8 
-            for i in range (1, length):
-                #loop through the next 12 hours or next 7 days
-
-                temporary = current[i]
-                loopList = [i]
-                if option == "daily":
-                    temp = temporary['temp']
-                    average = temperature_conversion(temp['day'])
-                    min = temperature_conversion(temp['min'])
-                    max = temperature_conversion(temp['min'])
-                    loopList.append(average)
-                    loopList.append(min)
-                    loopList.append(max)
-                else:
-                    temperature = temperature_conversion(temporary['temp'])
-                    loopList.append(temperature)
-                weather = temporary['weather']
-                description = weather[0]['description']
-                clouds = f"{temporary['clouds']}% clouds"
-                pop = f"{temporary['pop']*100}% chance of precipation"
-                loopList.append(description)
-                loopList.append(clouds)
-                loopList.append(pop)
-                currentList.append(loopList)
-            result.append(currentList)
-    except KeyError as e:
-        logging.error(f"{e} Weather API Error: Field {e} not available")
-        return [] 
-    except TypeError as e:
-        logging.error("Weather API Error: Field {e} returned None")
-        return []    
-    return result
     
 def start_text_interface(cfg):
     logging.info("Starting text interface. Type `stop` to finish")
@@ -201,7 +112,6 @@ if __name__ == "__main__":
     logging.info("Larisa is starting")
     config = configparser.ConfigParser()
     config.read('config.ini')
-    weather = weather_forecast(config,"Innopolis")
     if 'ui' not in config:
         logging.error("Config section [ui] is missing")
         exit(1)
--- logic/processor.py
+++ logic/processor.py
@@ -1,3 +1,6 @@
+import requests
+from geopy import Nominatim
+
 class Response:
     def __init__(self, text=None, payload=None, status='ok'):
         self.text = text
@@ -19,6 +22,8 @@ def _get_intent(text):
         return 'coffee'
     if 'translate' in text:
         return 'translate'
+    if 'weather' in text:
+        return 'weather'
 
     #TODO: implement
 
@@ -33,6 +38,94 @@ def _coffee(text):
 def _translate(text):
     return Response(text="")
 
+    
+def temperature_conversion(temp,logging):
+    '''
+    #Author: Nils Becker
+    Converts a given temperature in °K into a °C String representation
+    :param temp: Float or int Value of temperature in °K
+    :returns: The temperature converted to °C as a String
+    '''
+    try:
+        return f"{int(temp)-273}°C"
+    except ValueError:
+        logging.error(f"{temp} must be an int or float")
+        pass
+
+def _weather(cfg, city, logging):
+    '''
+    #Author: Nils Becker
+    Gives a weather forecast for a specified city
+    :param cfg: Currently used configuration
+    :param city: String representation of city
+    :returns: weather forecast for now/ next 12h/ next 7 days in a List
+    '''
+    locator = Nominatim(user_agent="Larissa")
+    location = locator.geocode(city)
+    #get key from https://openweathermap.org/
+    api_key = cfg['weather']['apikey']
+    try:
+        #create API request for given city
+        url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&appid={}'.format(location.latitude, location.longitude, "minutely,alerts", api_key)
+        response = requests.get(url).json()
+    except AttributeError:
+        logging.error(f"City {city} not found!")
+        return []
+    result = []
+    try:
+        #get data
+        current = response['current']
+        currentList = []
+        temperature = temperature_conversion(current['temp'], logging)
+        weather = current['weather']
+        description = weather[0]['description']
+        clouds = f"{current['clouds']}% clouds"
+        currentList.append(temperature)
+        currentList.append(description)
+        currentList.append(clouds)
+        result.append([currentList])
+        for option in ["hourly", "daily"]:
+            #get hourly, then daily data
+
+            currentList = []
+            current = response[option]
+            #if option == hourly, the next 12 hours are relevant -> length = 13
+            #if option == daily, the next 7 days are relevant -> length 8
+            length = 13 if option == "hourly" else 8 
+            for i in range (1, length):
+                #loop through the next 12 hours or next 7 days
+
+                temporary = current[i]
+                loopList = [i]
+                if option == "daily":
+                    temp = temporary['temp']
+                    average = temperature_conversion(temp['day'], logging)
+                    min = temperature_conversion(temp['min'], logging)
+                    max = temperature_conversion(temp['min'], logging)
+                    loopList.append(average)
+                    loopList.append(min)
+                    loopList.append(max)
+                else:
+                    temperature = temperature_conversion(temporary['temp'], logging)
+                    loopList.append(temperature)
+                weather = temporary['weather']
+                description = weather[0]['description']
+                clouds = f"{temporary['clouds']}% clouds"
+                pop = f"{temporary['pop']*100}% chance of precipation"
+                loopList.append(description)
+                loopList.append(clouds)
+                loopList.append(pop)
+                currentList.append(loopList)
+            result.append(currentList)
+    except KeyError as e:
+        logging.error(f"{e} Weather API Error: Field {e} not available")
+        return [] 
+    except TypeError as e:
+        logging.error("Weather API Error: Field {e} returned None")
+        return []    
+    return Response(text = result)
+
+
 
 def process(command, cfg, logging):
     logging.info(f"Processing command `{command}`")
@@ -41,5 +134,7 @@ def process(command, cfg, logging):
         return _coffee(command)
     elif 'translate' == intent:
         return _translate(text)
+    elif 'weather' == intent:
+        return _weather(cfg, "Innopolis", logging)
 
     return None
